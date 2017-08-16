13617d5c5b7199c5fae4112e7729d77fd962b795
jc428732 <connor.chivers@my.jcu.edu.au>
2017-08-17 09:26:29 +1000

    Refactored temperatures.py and broken_score.py.

--- Practical3/broken_score.py
+++ Practical3/broken_score.py
@@ -1,10 +1,13 @@
-score = float(input("Enter score: "))
+def score_to_grade(score):
+    if score > 100 or score < 0:
+        grade = "Invalid score."
+    elif score >= 90:
+        grade = "Excellent."
+    elif score >= 50:
+        grade = "Passable."
+    else:
+        grade = "Bad."
+    return grade
 
-if score > 100 or score < 0:
-    print("Invalid score.")
-elif score >= 90:
-    print("Excellent.")
-elif score >= 50:
-    print("Passable.")
-else:
-    print("Bad.")
+user_score = float(input("Enter score: "))
+result = score_to_grade(user_score)
--- Practical3/temperatures.py
+++ Practical3/temperatures.py
@@ -8,14 +8,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def celsius_to_fahrenheit(celsius_temperature):
+    fahrenheit_temperature = celsius_temperature * 9.0 / 5 + 32
+    return fahrenheit_temperature
+
+
+def fahrenheit_to_celsius(fahrenheit_temperature):
+    celsius_temperature = 5 / 9 * (fahrenheit - 32)
+    return celsius_temperature
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = celsius_to_fahrenheit(celsius)
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = fahrenheit_to_celsius(fahrenheit)
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
