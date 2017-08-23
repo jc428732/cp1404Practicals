87da5d32f89b6a978aaa2936f01d32c15705070f
jc428732 <connor.chivers@my.jcu.edu.au>
2017-08-23 13:43:44 +1000

    Add walk through and intermediate exercises to practical 4.

---
+++ Practical4/total_income.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+def print_report(time_period_type, number_of_time_periods, values):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for time_period in range(1, number_of_time_periods + 1):
+        value = values[time_period - 1]
+        total += value
+        print("{} {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(time_period_type, time_period, value, total))
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input("Enter income for month {}:".format(month)))
+        incomes.append(income)
+
+    print_report("Month", number_of_months, incomes)
+
+main()
