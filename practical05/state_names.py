26af323a49a35e80fc81fad8b7e67d0abf78912e
jc428732 <connor.chivers@my.jcu.edu.au>
2017-08-30 13:55:15 +1000

    Create feedback branch.

--- practical05/state_names.py
+++ practical05/state_names.py
@@ -9,13 +9,13 @@ STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Te
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
-# state = input("Enter short state: ").upper()
-# while state != "":
-#     if state in STATE_NAMES:
-#         print(state, "is", STATE_NAMES[state])
-#     else:
-#         print("Invalid short state")
-#     state = input("Enter short state: ").upper()
+state = input("Enter short state: ").upper()
+while state != "":
+    if state in STATE_NAMES:
+        print(state, "is", STATE_NAMES[state])
+    else:
+        print("Invalid short state")
+    state = input("Enter short state: ").upper()
 
-for abbreviation in STATE_NAMES:
-    print("{:>3} is {}".format(abbreviation, STATE_NAMES[abbreviation]))
+# for abbreviation in STATE_NAMES:
+#     print("{:>3} is {}".format(abbreviation, STATE_NAMES[abbreviation]))
