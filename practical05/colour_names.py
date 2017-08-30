9f5d42a39307f716cb697b66ce1854e7af304138
jc428732 <connor.chivers@my.jcu.edu.au>
2017-08-30 14:00:18 +1000

    Reduce line lengths to meet PEP8 standards.

--- practical05/colour_names.py
+++ practical05/colour_names.py
@@ -1,4 +1,6 @@
 
-colours = {"gray": "#bebebe", "gray1": "#030303", "gray10": "#1a1a1a", "gray11": "#1c1c1c", "gray12": "#1f1f1f", "gray13": "#212121", "gray14": "#242424", "gray15": "#262626", "gray16": "#292929", "gray17": "#2b2b2b", "gray18": "#2e2e2e", "gray19": "#303030"}
+colours = {"gray": "#bebebe", "gray1": "#030303", "gray10": "#1a1a1a", "gray11": "#1c1c1c", "gray12": "#1f1f1f",
+           "gray13": "#212121", "gray14": "#242424", "gray15": "#262626", "gray16": "#292929", "gray17": "#2b2b2b",
+           "gray18": "#2e2e2e", "gray19": "#303030"}
 colour = str(input("Enter a colour: "))
 print("{} is {}".format(colour, colours[colour]))
