90b2cf30be48b1fc16dca3d38871fcbcac6360d5
jc428732 <connor.chivers@my.jcu.edu.au>
2017-09-14 10:38:08 +1000

    Add practical 6.

--- practical06/KivyDemos-master/dynamic_list.py
+++ practical06/KivyDemos-master/dynamic_list.py
@@ -1,19 +1,46 @@
+"""
+Kivy example for CP1404/CP5632, IT@JCU
+Dynamically create buttons based on content of dictionary
+Lindsay Ward
+Modified from popup_demo, 11/07/2016
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Label
+from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
+__author__ = 'Lindsay Ward'
 
-class DynamicList(App):
+
+class DynamicWidgetsApp(App):
+    """ Main program - Kivy app to demo dynamic widget creation """
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """
+        Construct main app
+        """
+        super().__init__(**kwargs)
+        self.phonebook = ["!", "Yes", "8", "#"]
+
     def build(self):
-        Window.size = (1200, 400)
+        """
+        Build the Kivy GUI
+        :return: reference to the root Kivy widget
+        """
         self.title = "Dynamic List"
         self.root = Builder.load_file('dynamic_list.kv')
+        self.create_widgets()
         return self.root
 
     def create_widgets(self):
-        for entry in self.entries:
-            temp_label = Label(text=str(entry))
-            self.root.ids.box.add_widget(temp_label)
+        """
+        Create buttons from dictionary entries and add them to the GUI
+        :return: None
+        """
+        for name in self.phonebook:
+            temp_button = Button(text=name)
+            self.root.ids.entriesBox.add_widget(temp_button)
 
-DynamicList().run()
+DynamicWidgetsApp().run()
