9be9a855b4ea95e566eb760330d83790f77f3a8b
jc428732 <30378649+jc428732@users.noreply.github.com>
2017-10-18 20:48:38 +1000

    Fix files.

--- practical09/cleanup_files.py
+++ practical09/cleanup_files.py
@@ -11,7 +11,7 @@ def main():
     print("Current directory is", os.getcwd())
 
     # change to desired directory
-    os.chdir('Lyrics')
+    os.chdir('Lyrics/Lyrics')
     # print a list of all files (test)
     print(os.listdir('.'))
 
@@ -24,11 +24,10 @@ def main():
         # ignore directories, just process files
             if not os.path.isdir(filename):
                 new_name = get_fixed_filename(filename)
-                print(new_name)
 
             # Option 1: rename file to new name - in place
                 try:
-                    os.rename(filename, new_name)
+                    os.rename("{}/{}".format(dir_name, filename), "{}/{}".format(dir_name, new_name))
                 except FileExistsError:
                     pass
                 except FileNotFoundError:
@@ -39,11 +38,11 @@ def main():
 
             # Processing subdirectories using os.walk()
 
-            # os.chdir('..')  # .. means "up" one directory
-            # for dir_name, subdir_list, file_list in os.walk('.'):
-            #     print("In", dir_name)
-            #     print("\tcontains subdirectories:", subdir_list)
-            #     print("\tand files:", file_list)
+            #os.chdir('..')  # .. means "up" one directory
+            #for dir_name, subdir_list, file_list in os.walk('.'):
+                #print("In", dir_name)
+                #print("\tcontains subdirectories:", subdir_list)
+                #print("\tand files:", file_list)
 
 
 def get_fixed_filename(filename):
